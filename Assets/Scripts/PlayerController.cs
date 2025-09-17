5cbd69da8f50672d58f34e8850720429ea344445
runaarai <luna.alay1006@gmail.com>
2025-09-17 14:29:07 +0900

    Playerを動かすところまで

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
 
     [Header("プレイヤーの角度計算用")]
     public float angleZ = -90f; //プレイヤーの角度取得用
-    
+
     [Header("オン・オフの対象スポットライト")]
     public GameObject spotLight; //対象のスポットライト
 
@@ -29,13 +29,28 @@ public class PlayerController : MonoBehaviour
         //スポットライトを所持していればスポットライト表示
         if (GameManager.hasSpotLight)
         {
-          spotLight.SetActive(true); // 表示  
+            spotLight.SetActive(true); // 表示  
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move(); //上下左右の入力値の取得
+    }
+
+    private void FixedUpdate()
+    {
+        // 入力状況に応じてPlayerを動かす
+        rbody.linearVelocity = (new Vector2(axisH, axisV)).normalized * playerSpeed;
+
+    }
+
+    //上下左右の入力値の取得
+    public void Move()
+    {
+        //axisHとaxisVに入力状況を代入する
+        axisH = Input.GetAxisRaw("Horizontal"); // 左右キー
+        axisV = Input.GetAxisRaw("Vertical");   // 上下キー
     }
 }
