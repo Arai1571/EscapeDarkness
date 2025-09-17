e2f287798d12e1c2315b3a0e9e13d2ea42b3d4bc
runaarai <luna.alay1006@gmail.com>
2025-09-17 16:02:39 +0900

    angleZを利用してアニメーション追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,6 +37,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move(); //上下左右の入力値の取得
+        angleZ = GetAngle();//その時の角度を変数angleZに反映
+        Animation();  //angleZを利用してアニメーション
     }
 
     private void FixedUpdate()
@@ -52,5 +54,72 @@ public class PlayerController : MonoBehaviour
         //axisHとaxisVに入力状況を代入する
         axisH = Input.GetAxisRaw("Horizontal"); // 左右キー
         axisV = Input.GetAxisRaw("Vertical");   // 上下キー
+    }
+
+    //その時のプレイヤーの角度を取得するメソッド
+    public float GetAngle()
+    {
+        //現在座標の取得
+        Vector2 fromPos = transform.position;
+
+        //その瞬間のキー入力値（axisH,axisV）に応じた予測座標の取得
+        Vector2 toPos = new Vector2(fromPos.x + axisH, fromPos.y + axisV);
+
+        float angle;  //returnされる値の準備
+
+        //もしも何かしらの入力があれば新たに角度算出
+        if (axisH != 0 || axisV != 0)
+        {
+            // fromPos → toPos のベクトルを計算
+            float dirX = toPos.x - fromPos.x;
+            float dirY = toPos.y - fromPos.y;
+
+            // Atan2でラジアン角を取得 → 度に変換
+            angle = Mathf.Atan2(dirY, dirX) * Mathf.Rad2Deg;
+        }
+        //何も入力されていなければ前フレームの角度情報を据え置き
+        else
+        {
+            angle = angleZ;
+        }
+
+        return angle;
+    }
+
+    void Animation()
+    {
+        //何らかの入力がある場合
+        if (axisH != 0 || axisV != 0)
+        {
+            //ひとまずRunアニメを走らせる
+            anime.SetBool("run", true);
+
+            //angleZを利用して方角を決める.  パラメータdirection int型
+            //int型のdirection 下：０、上：１、右：２、左：それ以外(3)
+
+            if (angleZ > -135f && angleZ < -45f)  //下方向
+            {
+                anime.SetInteger("direction", 0);
+            }
+            else if (angleZ >= -45f && angleZ <= 45f) //右方向
+            {
+                anime.SetInteger("direction", 2);
+                transform.localScale = new Vector2(1, 1);
+            }
+            else if (angleZ > 45f && angleZ < 135f) //上方向
+            {
+                anime.SetInteger("direction", 1);
+            }
+            else //左方向
+            {
+                anime.SetInteger("direction", 3);
+                transform.localScale = new Vector2(-1, 1);
+            }
+        }
+
+        else//入力がない場合
+        {
+            anime.SetBool("run", false);  //走るフラグをOff
+        }
     }
 }
