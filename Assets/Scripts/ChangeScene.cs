bc843d352aaf2a7f6f78ab014c87ba041ae23c1b
runaarai <luna.alay1006@gmail.com>
2025-09-26 10:45:40 +0900

    ChangeSceneの作成と各オブジェクトの配置

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,16 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneName; 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
+
